5452dd15b98671e917e700b0ad6fe8d4ec2f1f63
roman-kashitsyn <roman.kashitsyn@gmail.com>
2012-04-25 18:51:08 +0400

    Add key mapping patterns support and new tests

--- redmate/rules.py
+++ redmate/rules.py
@@ -1,18 +1,28 @@
-class ToHashRule:
+class Db2RedisRule(object):
+    def __init__(self, *args, **kwargs):
+        self.table = kwargs.get("table")
+        self.query = kwargs.get("query")
+        if not self.table and not self.query:
+            raise ValueError("No either table or query is specified")
+        if not self.query:
+            self.query = "select * from {0}".format(self.table)
+
+class ToHashRule(Db2RedisRule):
 
     def __init__(self, *args, **kwargs):
-        self.kwargs = kwargs
+        super(ToHashRule, self).__init__(*args, **kwargs)
+        self.key_pattern = kwargs.get("key_pattern")
+        if not self.key_pattern:
+            raise ValueError("No key pattern specified")
 
     def run(self, db, redis):
-        rows = None
-        if "table" in self.kwargs:
-            rows = db.select(
-                "select * from {0}".format(self.kwargs["table"]), as_hash=True)
+        rows = db.select(query=self.query, as_hash=True)
         if not rows:
             return
-        key = self.kwargs["key_pattern"]
+        key_pattern = self.key_pattern
         pipe = redis.pipeline()
         for row in rows:
+            key = key_pattern.format(**row)
             pipe.hmset(key, row)
         pipe.execute()
 
--- test/testmapper.py
+++ test/testmapper.py
@@ -9,23 +9,34 @@ class MapperTest(unittest.TestCase):
         self.redis = Mock(name="redis-client-mock")
         self.mapper = redmate.Mapper(self.db, self.redis)
 
-    def test_map_single_row_from_to_hash(self):
+    def test_map_rows_to_hash(self):
         """
-        to_hash rule should map table with single row
-        to a hash with a single key.
+        to_hash rule should map table rows to a hash
+        using spedified pattern for keys
         """
-        row = {"id": 1, "field": "value"}
+        rows = ({"id": 1, "field": "abc"}, {"id": 2, "field": "bcd"})
 
-        self.db.select.return_value = [row]
+        self.db.select.return_value = rows
 
 
-        self.mapper.to_hash(table="MyTable", key_pattern="key")
+        self.mapper.to_hash(table="MyTable", key_pattern="row:{id}")
         self.mapper.run()
 
-        self.db.select.assert_called_with("select * from MyTable", as_hash=True)
+        self.db.select.assert_called_with(
+            query="select * from MyTable", as_hash=True)
         self.redis.pipeline.assert_called_with()
-        self.redis.pipeline().hmset.assert_called_with("key", row)
+        self.redis.pipeline().hmset.assert_any_call("row:1", rows[0])
+        self.redis.pipeline().hmset.assert_any_call("row:2", rows[1])
         self.redis.pipeline().execute.assert_called_with()
+
+    def test_to_hash_errors(self):
+        """
+        to_hash method should raise a ValueError if
+        request is not all parameters specified
+        """
+        self.assertRaises(ValueError, self.mapper.to_hash)
+        self.assertRaises(ValueError, self.mapper.to_hash, table="")
+        self.assertRaises(ValueError, self.mapper.to_hash, query="")
 
 if __name__ == "__main__":
     unittest.main()
